e023ede17b6e086488e94145f04e3cf4f48ccb81
Matías Reyes <matreyes@gmail.com>
2026-04-19 09:08:05 -0400

    Add checksum file for precompiled NIFs and fix macOS Intel runner

---
+++ checksum-Elixir.MinijinjaEx.NIF.exs
@@ -0,0 +1,10 @@
+%{
+  "libminijinja_ex-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" =>
+    "sha256:5657eb50c500001f16578cc33097ed07cb12f2bb3d6b99ff87860993ae56d512",
+  "libminijinja_ex-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:71c88383243b28343ee715555820d571196cae8d2ead0e7042c17ed2b1c86af1",
+  "libminijinja_ex-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:6a7c649ef93056c56076a2d3cadb3fb1621ae1888446c0c41890ffef6ab7015c",
+  "libminijinja_ex-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" =>
+    "sha256:ee0430e8813c29257131524b6db717b8124f8110c473799402ae371ef00e1123"
+}
