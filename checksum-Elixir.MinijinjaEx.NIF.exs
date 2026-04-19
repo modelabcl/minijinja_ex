f48077b68614e7ab376ef23c302d81938e9b2c77
Matías Reyes <matreyes@gmail.com>
2026-04-19 10:56:39 -0400

    Add CI-based hex.pm publishing, remove Intel Mac target (billing required)

--- checksum-Elixir.MinijinjaEx.NIF.exs
+++ checksum-Elixir.MinijinjaEx.NIF.exs
@@ -1,10 +1,1 @@
-%{
-  "libminijinja_ex-v0.1.2-nif-2.15-aarch64-apple-darwin.so.tar.gz" =>
-    "sha256:e7781601dad213d9848bfba0993e78f325cf6ae5e82b43e61c064bae9d43d7a9",
-  "libminijinja_ex-v0.1.2-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" =>
-    "sha256:c09fea35c97601bccc9e96e6d9e2c0a88abf79f14a93c0b9b53962ca67b6aef0",
-  "libminijinja_ex-v0.1.2-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" =>
-    "sha256:7a4190ecf73a129fd84aa047e460329db68fd209b623c892f2d33ed49d5d174d",
-  "libminijinja_ex-v0.1.2-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" =>
-    "sha256:5727f0e94de8bfa03181ae7d93277986d92f51e58f406d2927976f489c5ae204"
-}
+%{}
